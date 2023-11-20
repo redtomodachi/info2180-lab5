e5985118351a02949a94a53b958ff5c462bd1730
Kemar Wilson <sonic3554@gmail.com>
2023-11-19 21:11:08 -0500

    Exercise 5 was commited simple css code was made

--- world.js
+++ world.js
@@ -1,27 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() 
 {
     console.log("Page loaded");
-
+    var  city = document.getElementById("lookupcity");
     var look = document.getElementById("lookup");
-    look.addEventListener("click", search);
+    look.addEventListener("click", search.bind(this,"country"));
+    city.addEventListener("click", search.bind(this,"city"));
 });
 
-function search() 
+function search(query) 
 {
-    var getCountry = document.getElementById("result");
+    var getSearch = document.getElementById("result");
     var searchCountry = document.getElementById("country");
-
     var xammy = new XMLHttpRequest();
     xammy.onreadystatechange = function() {
         if (xammy.readyState === XMLHttpRequest.DONE) {
             if (xammy.status === 200) {
-                getCountry.innerHTML = xammy.responseText;
-            } else {
+                getSearch.innerHTML = xammy.responseText;
+            } 
+            else 
+            {
                 console.error("Error");
             }
         }
-    };
-
-    xammy.open("GET", `http://localhost/info2180-lab5/world.php?country=${searchCountry.value}`, true);
+    }
+    if(query == "country")
+    {
+        xammy.open("GET", "http://localhost/info2180-lab5/world.php?country="+ searchCountry.value, true);
+    }
+    else
+    {
+        xammy.open("GET","http://localhost/info2180-lab5/world.php?country=" + searchCountry.value + "&lookup=cities", false)
+    }
     xammy.send();
 }
--- world.php
+++ world.php
@@ -7,14 +7,37 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
+$city = filter_input(INPUT_GET, "lookup", FILTER_SANITIZE_STRING);
 $stmt = $conn->query("SELECT  countries.name, countries.continent,countries.independence_year,countries.head_of_state FROM countries WHERE countries.name LIKE '%$country%'");
+$city_stmt = $conn-> query("SELECT cities.name, cities.district, cities.population FROM cities LEFT JOIN countries ON countries.code = cities.country_code  WHERE countries.name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$results1 = $city_stmt -> fetchAll(PDO::FETCH_ASSOC);
 
 ?>
  
- <?php if(isset($country)): ?>
+ <?php if ($city == "cities"):?>
+  <table>
+    <thead>
+      <tr>
+        <th> Name </th>
+        <th> District </th>
+        <th> Population </th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($results1 as $lookup): ?>
+        <tr>
+          <td><?php print $lookup["name"]; ?></td>
+          <td><?php print $lookup["district"]; ?></td>
+          <td><?php print $lookup["population"]; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+  <?php endif; ?>
+  <?php if($city != "cities"): ?>
 
-<table class = "countryinfo">
+<table>
 
   <thead>
     <tr>
