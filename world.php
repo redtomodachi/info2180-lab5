1e861b87e4f1db64803768ae1940ed530b84868f
Kemar Wilson <sonic3554@gmail.com>
2023-11-19 19:34:17 -0500

    Exercise 4 was commited

--- world.php
+++ world.php
@@ -7,13 +7,32 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
+$stmt = $conn->query("SELECT  countries.name, countries.continent,countries.independence_year,countries.head_of_state FROM countries WHERE countries.name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+ 
+ <?php if(isset($country)): ?>
+
+<table class = "countryinfo">
+
+  <thead>
+    <tr>
+      <th> Name </th>
+      <th> Continent </th>
+      <th> Independence </th>
+      <th> Head of State </th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($results as $country): ?>
+      <tr>
+        <td><?php print $country["name"]; ?></td>
+        <td><?php print $country["continent"]; ?></td>
+        <td><?php print $country["independence_year"]; ?></td>
+        <td><?php print $country["head_of_state"]; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+<?php endif; ?>
